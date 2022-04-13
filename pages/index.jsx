33013a876b8f753fee84074863c2096561dc8bf6
Shan15Dev <shansai@bluewin.ch>
2022-04-13 21:05:44 +0200

    init

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+
+
+export default function Home() {
+  return (
+    <div>
+
+    </div>
+  )
+}
