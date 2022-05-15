63866d80fc2eb88dedcb92b65b778de09ff04a96
Shan15Dev <shansai@bluewin.ch>
2022-05-15 18:46:59 +0200

    menu navbar fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-export default function Navbar({ session }) {
+export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [blur, setBlur] = useState("");
 
@@ -16,7 +16,7 @@ export default function Navbar({ session }) {
     } else {
       setBlur("backdrop-blur-md");
     }
-  }, [session]);
+  }, []);
 
   return (
     <div
@@ -31,6 +31,51 @@ export default function Navbar({ session }) {
               alt="Logo of Shan"
             />
           </Link>
+          <div className="col-start-4">
+            {isOpen ? (
+              <button
+                className="outline-none mobile-menu-button"
+                onClick={handleMobileMenu}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            ) : (
+              <div className="md:hidden flex items-center">
+                <button
+                  className="outline-none mobile-menu-button md:hidden"
+                  onClick={handleMobileMenu}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 md:hidden"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
+          </div>
         </div>
 
         <ul
