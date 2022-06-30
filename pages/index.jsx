4b3b599792bb71f841c6de4224d5f06a154e4628
shan15dev <shansai@bluewin.ch>
2022-06-30 06:44:53 +0200

    Hover effect on tiles deleted for mobile version

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,15 @@
 
 export default function Home() {
     return (
-        <div className='flex text-white text-center justify-center mt-5 grid gap-y-10 md:grid-cols-9'>
-            <div className="block md:col-start-3 md:col-span-2 p-10 rounded-lg shadow-lg bg-slate-700 max-w-sm hover:scale-110 transition duration-500 cursor-default">
+        <div className='flex text-white text-center justify-center mt-5 grid gap-y-5  md:grid-cols-9'>
+            <div className="block md:col-start-3 md:col-span-2 p-10 rounded-lg shadow-lg bg-slate-700 max-w-sm md:hover:scale-110 transition duration-500 cursor-default">
                 <h1 className='text-white text-xl leading-tight font-medium mb-2'>Hello, I am Shan</h1>
                 <p className='text-white text-base mb-4'>I am a first year apprentice in Software Engineering. I am a full stack
                     developer. For frontend I use NextJS or ReactJS and for the backend I use C# or Java.</p>
             </div>
-            <div className="block md:col-start-6 md:col-span-2 p-10 rounded-lg shadow-lg bg-slate-700 max-w-sm hover:scale-110 transition duration-500 cursor-default">
+            <div className="block md:col-start-6 md:col-span-2 p-10 rounded-lg shadow-lg bg-slate-700 max-w-sm md:hover:scale-110 transition duration-500 cursor-default">
                 <h1 className='text-white text-xl leading-tight font-medium mb-2'>Current Projects</h1>
-                <p className='text-white text-base mb-4'>Currently I am working at a variaty of projects. The biggest project I am working at is a SchoolTool, where you can enter your grades and also note your important appointments and way more.</p>
+                <p className='text-white text-base mb-4'>Currently I am working at a variety of projects. The biggest project I am working at is a SchoolTool, where you can enter your grades and also note your important appointments and way more.</p>
             </div>
         </div>
     )
---
+++ pages/projects.jsx
@@ -0,0 +1,9 @@
+function Projects() {
+    return (
+        <div className="flex text-white text-center justify-center mt-5">
+            <h1 className>asfasdf</h1>
+        </div>
+    );
+}
+
+export default Projects;
